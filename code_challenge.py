b852850de82413ab4ca25c8f230404ff7fa45218
seph103124 <josephnardo255@gmail.com>
2026-08-18 15:26:46 +0800

    Add files via upload

---
+++ code_challenge.py
@@ -0,0 +1,14 @@
+name= '\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t*\t\t\t\t\t'
+sir= '\t\n\n\t\t\t\t\t\t\t\t\t\t\t\t\t\t*\t\t*\t\t\t\t'
+rovic= '\t\n\n\t\t\t\t\t\t\t\t\t\t\t\t\t*\t\t\t\t*\t'
+jet= '\t\n\n\t\t\t\t\t\t\t\t\t\t\t\t*\t\t\t\t\t\t*\t\t\t'
+shesh= '\t\n\n\t\t\t\t\t\t\t\t\t\t\t*\t\t\tHi, my name is\t\t\t\t*\t\t\t\t'
+wow= '\t\n\n\t\t\t\t\t\t\t\t\t\t\t\t*\t\t    Nardo\t\t\t*\t\t\t\t'
+fd= '\t\n\n\t\t\t\t\t\t\t\t\t\t\t\t\t*\t\t\t\t*\t'
+fgdf='\t\n\n\t\t\t\t\t\t\t\t\t\t\t\t\t\t*\t\t*\t\t\t\t'
+gf= '\t\n\n\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t*\t\t'
+
+
+
+
+print(name,sir,rovic,jet,shesh,wow,fd,fgdf,gf)
