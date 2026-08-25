136f08c6f0455992e563146ec65632f760c614ca
seph103124 <josephnardo255@gmail.com>
2026-08-25 12:51:01 +0800

    Add files via upload

---
+++ activity3.py
@@ -0,0 +1,27 @@
+#integer variable
+myAges = 18
+people = 24
+year = 2026
+
+#float variable
+myWeight = 50.6
+price = 61.3
+kilometer = 32.56
+
+#string variable
+myAddress = 'prk. sariling atin'
+name = "johnmarkmendoza_33"
+gmail = "gandanan233@gmail.com"
+
+#boolean variable
+isYounges = True
+isJoker = False
+isGood = False
+
+
+print("My Name is ",name)
+print("My Age is ",myAges, "years old")
+print("my Address is ",myAddress)
+print("Year now is ",year)
+print("Price of fish ",price)
+print("population ",people,)
