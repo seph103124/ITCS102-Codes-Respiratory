ca9d00b473f5619e54112469a678592dce956bdd
seph103124 <josephnardo255@gmail.com>
2026-08-30 15:18:03 +0800

    Add files via upload

--- code_challenge5.py
+++ code_challenge5.py
@@ -1,41 +1,34 @@
-money = 2343
+money = 8678
 
 print("Money to Deposit -->", money)
 
 a1000 = money // 1000
 money = money % 1000
-
-a500 = money // 500
+b500 = money // 500
 money = money % 500
-
-a200 = money // 200
+c200 = money // 200
 money = money % 200
-
-a100 = money // 100
+d100 = money // 100
 money = money % 100
-
-a50 = money // 50
+e50 = money // 50
 money = money % 50
-
-a20 = money // 20
+f20 = money // 20
 money = money % 20
-
-a10 = money // 10
+g10 = money // 10
 money = money % 10
-
-a5 = money // 5
+h5 = money // 5
 money = money % 5
-
-a1 = money // 1 
+i1 = money // 1 
 money = money % 1
 
 print("1000 =", a1000)
-print("500 =", a500)
-print("200 =", a200)
-print("100 =", a100)
-print("50 =", a50)
-print("20 =", a20)
-print("10 =", a10)
-print("1 =", a1)
+print("500 =", b500)
+print("200 =", c200)
+print("100 =", d100)
+print("50 =", e50)
+print("20 =", f20)
+print("10 =", g10)
+print("5 =", h5)
+print("1 =", i1)
 
  
