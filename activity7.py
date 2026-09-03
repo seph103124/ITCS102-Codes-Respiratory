929d6c423a5fdb9a19b827350d63788a07b2d8ff
seph103124 <josephnardo255@gmail.com>
2026-09-03 12:11:16 +0800

    Add files via upload

---
+++ activity7.py
@@ -0,0 +1,33 @@
+total = 0
+
+n1 = eval(input("Enter a number --> "))
+total = total + n1
+
+#repeat input 10x, explore other assignment
+#operator
+n2 = eval(input("Enter a number --> "))
+total = total + n2
+
+n2 = eval(input("Enter a number --> "))
+total += n2
+
+n2 = eval(input("Enter a number --> "))
+total -= n2
+
+n2 = eval(input("Enter a number --> "))
+total *= n2
+
+n2 = eval(input("Enter a number --> "))
+total /= n2
+
+n2 = eval(input("Enter a number --> "))
+total += n2
+
+n2 = eval(input("Enter a number --> "))
+total -= n2
+
+n2 = eval(input("Enter a number --> "))
+total *= n2
+
+print("The sumis ", total)
+
