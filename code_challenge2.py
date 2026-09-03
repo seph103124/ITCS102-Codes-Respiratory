54924247f51d718b9647c0dcd7d36a01317b73bc
seph103124 <josephnardo255@gmail.com>
2026-09-03 12:14:24 +0800

    Add files via upload

---
+++ code_challenge2.py
@@ -0,0 +1,24 @@
+money = 2345
+
+print("Money to Deposit -->", money)
+
+rovic = money // 1000
+jet = money % 1000 // 500
+marbert = money % 500 // 200
+nards = money % 200 // 100
+alden = money % 100 // 50
+jl = money % 50 // 20
+joshua = money % 20 // 10
+piolo = money % 10 // 5
+jerico = money % 5 // 1
+
+
+print("1000 =", rovic)
+print("500 =", jet)
+print("200 =", marbert)
+print("100 =", nards)
+print("50 =", alden)
+print("20 =", jl)
+print("10 =", joshua)
+print("5 =", piolo)
+print("1 =", jerico)
