a310630aa6187e86d8305849090b24ff8bb658a8
seph103124 <josephnardo255@gmail.com>
2026-09-08 12:30:00 +0800

    Add files via upload

---
+++ activity11.py
@@ -0,0 +1,10 @@
+username = 'user1'
+password = 'araykopo23'
+
+u = input('input Username ---> ')
+p = input('input Password ---> ')
+
+if username == u and p == password :
+	print("ACCESS GRANTED")
+else :
+	print("ACCESS DENIED")
