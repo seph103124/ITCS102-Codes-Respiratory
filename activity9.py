3fc667212d07be491bb74bd2d3ea7642189d2d23
seph103124 <josephnardo255@gmail.com>
2026-09-08 12:02:28 +0800

    Add files via upload

---
+++ activity9.py
@@ -0,0 +1,20 @@
+x = 5
+y = 5
+
+text1 = "sample"
+text2 = "Sample"
+
+#1
+print( x > y )
+#2
+print( x < y )
+#3
+print( x >= y )
+#4
+print( x <= y )
+#5
+print( x == y )
+#6
+print( text1 == text2 )
+#7
+print( text1 != text2 )
