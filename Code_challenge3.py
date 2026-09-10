2f2f5c36d716de02d7b2da22e7fc31628ac16bda
seph103124 <josephnardo255@gmail.com>
2026-09-10 18:27:17 +0800

    Create Code_challenge3.py

---
+++ Code_challenge3.py
@@ -0,0 +1,48 @@
+name = input("sender name ---> ")
+item = input("what kind of items ---> ")
+is_fragile = bool(input("it is fragile (if is not press enter) --->"))
+weight = float(input("what is weight? (kg) --->"))
+distance = float(input("how far in warehouse (km) --->"))
+is_express = bool(input("it is express (press enter if not) --->"))
+is_international = bool(input("international (press enter if not) --->"))
+
+#calculate cost base
+
+base_cost = (weight * 2.5) + (distance * 0.15)
+
+#evaluate pricing tiers (apply the first matching only)
+
+#free shipping
+
+if weight <= 0 and distance <= 100 and is_express == False and is_international == False:
+    print("Free shipping")
+    total = 0
+
+#international express
+
+elif is_international == True and is_express == True:
+    print("Parcel express to international applied")
+    Total = (base_cost * 1.4) + 50
+    
+#express or heavy international
+
+elif is_express == True or is_international == True and weight > 25:
+    print("Parcel is express or heavy international applied")    
+    Total = (base_cost * 1.20) + 25
+    
+#Oversized
+
+elif weight > 25 or distance > 1000:
+    print("Oversize applied")
+    Total = base_cost + 30
+    
+#Standard rate
+
+else:
+    print("standard rate is applied")    
+    Total = base_cost
+      
+      
+    print("Name of sender : ", name)
+    print("Name of item : ", item)
+    print("Over all total : peso ", Total)
